52bbbbdfe162c0754b6a5e5e74fdc9118872e599
Randy Kamindo <randykamindo@gmail.com>
2024-08-08 19:27:28 -0400

    Initial implementation of concurrent request handling using epoll for efficient I/O multiplexing and scalable connection management
    The following changes are made to this commit
    1. Implement handling concurrent requests, supporting approx 30,000 requests per second
    
    Note:
    The main bottleneck to be addressed is that we are synchronously processing a command and sending a response to the client socket. Send is blocking the main thread until the response is sent. During that time, the thread cannot process other commands. To resolve this, consider appending responses to a queue that is processed by other threads.
    
    #9

--- src/server.c
+++ src/server.c
@@ -1,8 +1,12 @@
 #include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "khash.h"
@@ -10,6 +14,7 @@
 
 #define DEFAULT_PORT 6379
 #define BUFFER_SIZE 1024
+#define MAX_EVENTS 50
 
 typedef enum { TYPE_STRING } ValueType;
 
@@ -106,74 +111,114 @@ int start_server() {
     exit(EXIT_FAILURE);
   }
 
-  printf("server started listening");
+  printf("server started listening\n");
+
+  int epfd = epoll_create(1);
+  if (epfd == -1) {
+    perror("epoll_create1 failed");
+    exit(EXIT_FAILURE);
+  }
+
+  struct epoll_event event;
+  event.events = EPOLLIN;  // EPOLLIN is the flag for read events
+  event.data.fd = SocketFD;
+
+  if (epoll_ctl(epfd, EPOLL_CTL_ADD, SocketFD, &event) == 1) {
+    perror("epoll_ctl failed");
+    exit(EXIT_FAILURE);
+  }
+
+  struct epoll_event events[MAX_EVENTS];
+  int num_events;
 
   for (;;) {
-    int ConnectFD = accept(SocketFD, NULL, NULL);
-
-    if (ConnectFD == -1) {
-      perror("accept failed");
-      close(SocketFD);
+    num_events = epoll_wait(epfd, events, MAX_EVENTS, -1);
+    if (num_events == -1) {
+      perror("epoll_wait failed");
       exit(EXIT_FAILURE);
     }
 
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_received;
-
-    // read from the client
-    bytes_received = recv(ConnectFD, buffer, BUFFER_SIZE - 1, 0);
-    if (bytes_received > 0) {
-      buffer[bytes_received] = '\0';  // null terminate the received string
-      printf("Received: %s\n", buffer);
-
-      // deserialize received RESP data
-      int count;
-      char **parsed_command = deserialize_command(buffer, &count);
-      if (parsed_command && count > 0) {
-        const char *response;
-        // check if the received message is ping
-        if (strcmp(parsed_command[0], "PING") == 0) {
-          response = serialize_simple_string("PONG");
-        } else if (strcmp(parsed_command[0], "ECHO") == 0 && count > 1) {
-          response = serialize_bulk_string(parsed_command[1]);
-        } else if (strcmp(parsed_command[0], "SET") == 0 && count > 2) {
-          set_value(h, parsed_command[1], parsed_command[2], TYPE_STRING);
-          response = serialize_simple_string("OK");
-        } else if (strcmp(parsed_command[0], "GET") == 0 && count > 1) {
-          RedisValue *redis_value = get_value(h, parsed_command[1]);
-          if (redis_value == NULL) {
-            response = serialize_bulk_string(NULL);
-          } else {
-            response = serialize_bulk_string(redis_value->data.str);
-          }
-        } else {
-          response = serialize_error("ERR unknown command");
+    // iterate through the events
+    for (int i = 0; i < num_events; i++) {
+      if (events[i].data.fd ==
+          SocketFD) {  // server socket is ready, new connection
+        int ConnectFD = accept(SocketFD, NULL, NULL);
+        if (ConnectFD == -1) {
+          perror("accept failed");
+          close(SocketFD);
+          exit(EXIT_FAILURE);
         }
-        if (response) {
-          ssize_t bytes_sent = send(ConnectFD, response, strlen(response), 0);
-          if (bytes_sent < 0) {
-            perror("send failed");
-          } else {
-            printf("Sent: %s\n", response);
-          }
+
+        int optval = 1;
+        setsockopt(ConnectFD, IPPROTO_TCP, TCP_NODELAY, &optval,
+                   sizeof(optval));
+
+        event.events = EPOLLIN;
+        event.data.fd = ConnectFD;
+
+        if (epoll_ctl(epfd, EPOLL_CTL_ADD, ConnectFD, &event) == -1) {
+          perror("epoll_ctl failed");
+          exit(EXIT_FAILURE);
         }
-        // free the deserialized command
-        free_command(parsed_command, count);
       } else {
-        printf("Invalid command received\n");
-      }
-    } else if (bytes_received == 0) {
-      printf("Client disconnected\n");
-    } else {
-      printf("read failed");
-    }
+        char buffer[BUFFER_SIZE];
+        ssize_t bytes_received;
 
-    // if (shutdown(ConnectFD, SHUT_RDWR) == -1) {
-    //   perror("shutdown failed");
-    // }
-    close(ConnectFD);
+        // read from the client
+        bytes_received = recv(events[i].data.fd, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes_received > 0) {
+          buffer[bytes_received] = '\0';  // null terminate the received string
+          printf("Received: %s\n", buffer);
+
+          // Check for PING command
+          if (strcmp(buffer, "PING\r\n") == 0 ||
+              strcmp(buffer, "*1\r\n$4\r\nPING\r\n") == 0) {
+            const char *response = serialize_simple_string("PONG");
+            send_response(events[i].data.fd, response);
+          } else {
+            // deserialize received RESP data
+            int count;
+            char **parsed_command = deserialize_command(buffer, &count);
+            if (parsed_command && count > 0) {
+              const char *response;
+              if (strcmp(parsed_command[0], "ECHO") == 0 && count > 1) {
+                response = serialize_bulk_string(parsed_command[1]);
+              } else if (strcmp(parsed_command[0], "SET") == 0 && count > 2) {
+                set_value(h, parsed_command[1], parsed_command[2], TYPE_STRING);
+                response = serialize_simple_string("OK");
+              } else if (strcmp(parsed_command[0], "GET") == 0 && count > 1) {
+                RedisValue *redis_value = get_value(h, parsed_command[1]);
+                if (redis_value == NULL) {
+                  response = serialize_bulk_string(NULL);
+                } else {
+                  response = serialize_bulk_string(redis_value->data.str);
+                }
+              } else {
+                response = serialize_error("ERR unknown command");
+              }
+              if (response) {
+                send_response(events[i].data.fd, response);
+              }
+              // free the deserialized command
+              free_command(parsed_command, count);
+            } else {
+              printf("Invalid command received\n");
+            }
+          }
+        } else if (bytes_received == 0) {
+          printf("Client disconnected\n");
+          close(events[i].data.fd);
+          epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+        } else {
+          printf("read failed");
+          close(events[i].data.fd);
+          epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+        }
+      }
+    }
   }
 
+  close(epfd);
   close(SocketFD);
   cleanup_hash(h);
   return EXIT_SUCCESS;
